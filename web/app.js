edcba77095a4abf8179b949d06b32963c3f0bd74
Ana C. Silva <carolokausp@gmail.com>
2017-08-11 16:30:24 -0300

    add span to the slider, add for to html

--- web/app.js
+++ web/app.js
@@ -8,20 +8,31 @@
 
     var url ="http://www.splashbase.co/api/v1/images/latest"
     $.getJSON(url)
+
+
     .done(function(json){
-      console.log(json.images.length);
-      for (i = 0; i < json.images.length; i++){
-          var img = json.images[i].url;
-          img = "http://c3uo9imc.cloudimg.io/width/200/none/" + img;
+      //json returns {images: [{url: '...'}]}
+      console.log(json);
 
-           html = template.render({link:img})
-          console.log(html);
+      html = template.render(json)
+      container.html(html);
 
-          container.append(html);
 
-          //   container.append("<div><img src=\""+img+"\"></div>");
-          //     console.log(json);
-      }
+
+
+      // console.log(json.images.length);
+      // for (i = 0; i < json.images.length; i++){
+      //     var img = json.images[i].url;
+      //     img = "http://c3uo9imc.cloudimg.io/width/200/none/" + img;
+      //
+      //      html = template.render({link:img})
+      //     console.log(html);
+      //
+      //     container.append(html);
+      //
+      //     //   container.append("<div><img src=\""+img+"\"></div>");
+      //     //     console.log(json);
+      // }
       container.slick(
         {
           slidesToShow: 3,
