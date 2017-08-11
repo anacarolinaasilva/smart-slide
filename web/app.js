e45cc740c7652ce2ed1556e6ce61410fb49fa68d
Ana C. Silva <carolokausp@gmail.com>
2017-08-11 16:14:38 -0300

    slider is working with template

--- web/app.js
+++ web/app.js
@@ -2,6 +2,8 @@
 
   $(document).ready(function(){
 
+    var template = $('#template-slide');
+
     var container = $('#images');
 
     var url ="http://www.splashbase.co/api/v1/images/latest"
@@ -10,9 +12,15 @@
       console.log(json.images.length);
       for (i = 0; i < json.images.length; i++){
           var img = json.images[i].url;
-          img = "http://c3uo9imc.cloudimg.io/width/200/none/" + img ;
-            container.append("<div><img src=\""+img+"\"></div>");
-              console.log(json);
+          img = "http://c3uo9imc.cloudimg.io/width/200/none/" + img;
+
+           html = template.render({link:img})
+          console.log(html);
+
+          container.append(html);
+
+          //   container.append("<div><img src=\""+img+"\"></div>");
+          //     console.log(json);
       }
       container.slick(
         {
