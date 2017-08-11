ee28fde1bb82c0143c4e3549e48fa8fd0bebf2fe
Ana C. Silva <carolokausp@gmail.com>
2017-08-11 15:46:05 -0300

    first commit

---
+++ web/app.js
@@ -0,0 +1,39 @@
+
+
+  $(document).ready(function(){
+
+    var container = $('#images');
+
+    var url ="http://www.splashbase.co/api/v1/images/latest"
+    $.getJSON(url)
+    .done(function(json){
+      console.log(json.images.length);
+      for (i = 0; i < json.images.length; i++){
+          var img = json.images[i].url;
+          img = "http://c3uo9imc.cloudimg.io/width/200/none/" + img ;
+            container.append("<div><img src=\""+img+"\"></div>");
+              console.log(json);
+      }
+      container.slick(
+        {
+          slidesToShow: 3,
+          adaptiveHeight: true,
+          dots: true
+        }
+      );
+
+    });
+/*
+  var container = document.getElementById('images');
+  var url ="http://www.splashbase.co/api/v1/images/latest"
+  $.getJSON(url)
+  .done(function(json){
+    for (i = 0; i < json.images.length; i++){
+      var img = json.images[i].url;
+      img = "http://c3uo9imc.cloudimg.io/width/200/none/" + img ;
+      img = "<img src=\""+img+"\">";
+      container.innerHTML += img;
+    }
+  });*/
+
+  });
