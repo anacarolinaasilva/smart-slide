73208e4698a9c2d88425b43292315df11f48270f
Ana C. Silva <carolokausp@gmail.com>
2017-09-01 13:05:14 -0400

    working slide vs non-working slide

--- web/app2.js
+++ web/app2.js
@@ -23,8 +23,16 @@ appmodel = function(){
    //console.log(self.slides());
 
       self.slides().forEach(function(item){
+        // item.removeimg2 = function(){};
         self.imageIds.push(item.id);
       });
+
+      // for(var i = 0; self.slides().length; i++){
+      //   self.slides()[i].removeimg2 = function(){
+      //   alert('removendo');
+      //  }
+      // }
+
 
   });
 
@@ -45,16 +53,26 @@ console.log(self.imageIds());
   }
 
   self.removeimg = function(){
-    image = this; //????
-    var doRemove = function(){
-        self.slides.remove(image);
-    }
+    image = this;
+    console.log('START image');
+    console.log(image);
+    console.log('END image');
+
+
+      var doRemove = function(){
+          self.slides.remove(image);
+      }
+
     swal({
       title: "Are you sure",
       type: "warning",
       showCancelButton: true,
     }, doRemove);
+
  }
+
+
+ //self.removeimg2 = function(){};
 
  self.removeImgById = function(){
  //image = this; //????
@@ -73,6 +91,7 @@ console.log(self.imageIds());
    if (!flagDeleted) {
      swal("Image Not Found", "", "warning")
    }
+
  }
 
 
@@ -80,6 +99,50 @@ console.log(self.imageIds());
 } //End appmodel
 
 
+/*Slider function [It is recommended to place a function in a separate JS file, such as "functions.js"]*/
+function myCoolSlider() {
+        $('#featured-content').slick({
+            dots: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    infinite: true,
+                    dots: true
+                }
+            }, {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            }, {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            }, {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }]
+        });
+    }
+    /*End of Slider function*/
+
 $(document).ready(function(){
-  ko.applyBindings(new appmodel())
+  /*Calling the function [It is recommended to call a function in a separate JS file, such as "scripts.js"]*/
+      myCoolSlider();
+  /*.................End of call*/
+
+  ko.applyBindings(new appmodel());
+
 });
